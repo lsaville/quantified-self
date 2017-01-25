0130fd129fb12b876ef051602a0cc69b49e69e6b
Lee Saville <l.w.saville@gmail.com>
2017-01-24 20:49:37 -0700

    Add prototype method to store an exercise

--- exercise.js
+++ exercise.js
@@ -3,4 +3,15 @@ function Exercise(name, calories) {
   this.calories = calories;
 }
 
+Exercise.prototype.store = function () {
+  var exercisesJSON = localStorage.getItem('exercises');
+  if (!exercisesJSON) {
+    exercisesJSON = '[]'
+  }
+  var exercises = JSON.parse(exercisesJSON);
+  exercises.push({name: this.name, calories: this.calories})
+  exercisesJSON = JSON.stringify(exercises);
+  localStorage.setItem('exercises', exercisesJSON);
+};
+
 module.exports = Exercise;
