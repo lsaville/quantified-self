f647f06e628589c2527eacfbfcfba021f910ea3a
Lee Saville <l.w.saville@gmail.com>
2017-01-26 20:51:14 -0700

    Add testing for adding names to input fields, creating exercise, my excercises existing on site visit, field validation and not creating a exercise if the field is invalid

--- test/exercise.js
+++ test/exercise.js
@@ -1,4 +1,5 @@
 var assert     = require('chai').assert;
+var expect     = require('chai').expect;
 var webdriver  = require('selenium-webdriver');
 var test       = require('selenium-webdriver/testing');
 
@@ -16,7 +17,7 @@ test.describe('testing exercises.html', function() {
     driver.quit();
   })
 
-  test.it('should allow me to add a name and calories for an exercise', function(){
+  test.xit('should allow me to add a name and calories for an exercise', function(){
     driver.get('http://localhost:8080/exercises.html');
 
     var name     = driver.findElement({id: 'exercise-name-input'})
@@ -35,10 +36,69 @@ test.describe('testing exercises.html', function() {
     });
   });
 
-  test.it('should allow me to create an exercise', function(){
+  test.xit('should allow me to create an exercise', function(){
     driver.get('http://localhost:8080/exercises.html');
 
-    var name     = driver.findElement({id: 'exercise-name-input'})
-    var calories = driver.findElement({id: 'exercise-calories-input'})
+    var name     = driver.findElement({id: 'exercise-name-input'});
+    var calories = driver.findElement({id: 'exercise-calories-input'});
+    var submitButton   = driver.findElement({id: 'add-exercise'});
+
+    name.sendKeys('running');
+    calories.sendKeys('300');
+    submitButton.click();
+
+    driver.findElement({css: '#exercise-table tbody tr td:nth-child(1)'}).getText().then(function(textValue) {
+      assert.equal(textValue, 'running');
+    })
+
+    driver.findElement({css: '#exercise-table tbody tr td:nth-child(2)'}).getText().then(function(textValue) {
+      assert.equal(textValue, '300');
+    })
+  });
+
+  test.xit('my exercises exist when I visit the site', function(){
+    driver.get('http://localhost:8080/exercises.html');
+
+    var data = JSON.stringify([{name: 'running', calories: '300'}]);
+
+    driver.executeScript("window.localStorage.setItem('exercises','" + data+ "');");
+
+    driver.get('http://localhost:8080/exercises.html');
+
+    driver.findElement({css: '#exercise-table tbody tr td:nth-child(1)'}).getText().then(function(textValue) {
+      assert.equal(textValue, 'running');
+    })
+
+    driver.findElement({css: '#exercise-table tbody tr td:nth-child(2)'}).getText().then(function(textValue) {
+      assert.equal(textValue, '300');
+    })
+  });
+
+  test.xit('the fields are validated', function(){
+    driver.get('http://localhost:8080/exercises.html');
+
+    var name           = driver.findElement({id: 'exercise-name-input'});
+    var submitButton   = driver.findElement({id: 'add-exercise'});
+
+    name.sendKeys('running');
+    submitButton.click();
+
+    driver.findElement({css: '.form-error'}).getText().then(function(textValue) {
+      assert.equal(textValue, 'Please enter a calorie amount');
+    })
+  });
+
+  test.it("doesn't allow an exercise to be created if a field is invalid", function(){
+    driver.get('http://localhost:8080/exercises.html');
+
+    var name           = driver.findElement({id: 'exercise-name-input'});
+    var submitButton   = driver.findElement({id: 'add-exercise'});
+
+    name.sendKeys('running');
+    submitButton.click();
+
+    driver.findElement({id: 'table-body'}).getText().then(function(value){
+      expect(value).to.be.empty;
+    });
   });
 });
