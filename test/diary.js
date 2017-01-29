000958799b5ce239c3c4669c4ba50a926c06faa5
Lee Saville <l.w.saville@gmail.com>
2017-01-29 09:57:03 -0700

    Create test file for testing diary.html with initial test for the appearance of today's date

---
+++ test/diary.js
@@ -0,0 +1,30 @@
+var assert    = require('chai').assert;
+var expect    = require('chai').expect;
+var webdriver = require('selenium-webdriver');
+var test      = require('selenium-webdriver/testing');
+
+test.describe('testing diary.html', function() {
+  var driver;
+  this.timeout(10000);
+
+  test.beforeEach(function() {
+    driver = new webdriver.Builder()
+      .forBrowser('chrome')
+      .build();
+  })
+
+  test.afterEach(function() {
+    driver.quit;
+  })
+
+  test.it('shows me todays date', function() {
+    driver.get('http://localhost:8080/');
+
+    var dateOnPage = driver.findElement({id: 'date'});
+    var dateToday  = new Date;
+
+    name.getAttribute('value').then(function(value) {
+      assert.equal(value, date.Today.toDateString())
+    })
+  })
+})
