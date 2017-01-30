12616dd146a6be8a39a3a9d50233df94730bb7ca
Lee Saville <l.w.saville@gmail.com>
2017-01-30 06:54:24 -0700

    X out tests and write new test around total calories"

--- test/diary.js
+++ test/diary.js
@@ -5,7 +5,7 @@ var test      = require('selenium-webdriver/testing');
 
 test.describe('testing diary.html', function() {
   var driver;
-  this.timeout(10000);
+  this.timeout(1000000);
 
   test.beforeEach(function() {
     driver = new webdriver.Builder()
@@ -17,7 +17,7 @@ test.describe('testing diary.html', function() {
     driver.quit();
   })
 
-  test.it('shows me todays date', function() {
+  test.xit('shows me todays date', function() {
     driver.get('http://localhost:8080/');
 
     var dateOnPage = driver.findElement({id: 'date'});
@@ -28,7 +28,7 @@ test.describe('testing diary.html', function() {
     })
   })
 
-  test.it('the day changes to yesterday if I click the left arrow', function(){
+  test.xit('the day changes to yesterday if I click the left arrow', function(){
     driver.get('http://localhost:8080/');
 
     var leftArrow  = driver.findElement({id: 'day-back'});
@@ -42,17 +42,38 @@ test.describe('testing diary.html', function() {
     });
   })
 
-  test.it('the day changes to tomorrow if I click the right arrow', function(){
+  test.xit('the day changes to tomorrow if I click the right arrow', function(){
     driver.get('http://localhost:8080/');
 
     var rightArrow  = driver.findElement({id: 'day-forward'});
-    var tomorrow  = new Date;
-    tomorrow      = new Date(tomorrow.setDate(tomorrow.getDate() + 1));
-    tomorrow      = tomorrow.toDateString();
+    var tomorrow    = new Date;
+    tomorrow        = new Date(tomorrow.setDate(tomorrow.getDate() + 1));
+    tomorrow        = tomorrow.toDateString();
     rightArrow.click()
 
     driver.findElement({id: 'date'}).getText().then(function(value) {
       assert.equal(value, tomorrow);
     });
+  })
+
+  test.it('has total calories for breakfast', function(){
+    driver.get('http://localhost:8080/');
+
+    var data = JSON.stringify([{name: 'banana', calories: '30'}, {name: 'Chocolate', calories: '400'}]);
+    driver.executeScript("window.localStorage.setItem('foods','" +data+ "');");
+
+    driver.get('http://localhost:8080/');
+// driver.sleep(1000000)
+    addToBreakfastButton  = driver.findElement({id: '#add-to-breakfast'})
+    bananaCheckbox        = driver.findElement({id: 'input#banana'});
+    chocolateCakeCheckbox = driver.findElement({id: 'input#Chocolate'});
+
+    bananaCheckbox.click();
+    chocolateCakeCheckbox.click();
+    addToBreakfastButton.click();
+
+    driver.findElement({css: '#breakfast-total-calories'}).getText().then(function(value){
+      assert.equal(value, '430');
+    })
   })
 })
